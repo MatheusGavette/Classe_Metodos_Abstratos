c25ec5736881ae38b34653c48372e805d8dfb2f5
MatheusGavette <matheus.gvtt10@gmail.com>
2019-05-25 14:55:51 -0300

    Finalização do programa

--- Classe_Metodos_Abstratos/Program.cs
+++ Classe_Metodos_Abstratos/Program.cs
@@ -10,7 +10,44 @@ namespace Classe_Metodos_Abstratos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Shape> colors = new List<Shape>();
+            Console.Write("Enter the number of shapes:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\n -------------- \n");
+
+                Console.Write("Rectangle or Circle (R/C)? ");
+                char opc = char.Parse(Console.ReadLine().ToUpper());
+
+                Console.Write("Color (Black/Blue/Red: ");
+                string color = Console.ReadLine();
+
+                if (opc == 'R')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Height: ");
+                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    colors.Add(new Rectangle(width, height, Enum.Parse<Color>(color)));
+                }
+                else
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    colors.Add(new Circle(radius, Enum.Parse<Color>(color)));
+                }
+            }
+
+            Console.WriteLine("\n SHAPE AREAS \n");
+
+            foreach (var s in colors)
+            {
+                Console.WriteLine(s.Area().ToString("F3", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
